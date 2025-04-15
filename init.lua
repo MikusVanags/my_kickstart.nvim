118f922c3072cc53d3c69dc4cdaf9260a8e7b72d
Mikus Vanags <mikus.vanags@teliacompany.com>
2025-04-15 11:41:53 +0000

    Copilot + removed eslint

--- init.lua
+++ init.lua
@@ -638,7 +638,7 @@ require('lazy').setup({
           filetypes = { 'component' },
         },
 
-        eslint = {},
+        -- eslint = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -1108,6 +1108,23 @@ require('lazy').setup({
         user_default_options = { mode = 'background' },
       }
     end,
+  },
+
+  -- Copilot stuff
+  {
+    'github/copilot.vim',
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
