68ddbef79399b3f6b446835f3b4126eff27dbc45
Mikus Vanags <mikus.vanags@teliacompany.com>
2025-09-19 12:15:26 +0300

    Removed Code Companion. Using sst/opencode cli anyway

--- init.lua
+++ init.lua
@@ -1202,27 +1202,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
 
-  {
-    'olimorris/codecompanion.nvim',
-    opts = {
-      strategies = {
-        chat = {
-          adapter = 'copilot',
-        },
-        inline = {
-          adapter = 'copilot',
-        },
-        agent = {
-          adapter = 'copilot',
-        },
-      },
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-  },
-
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
